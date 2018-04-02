e17073fc267c16f4b6faf9f6c0a36540c2722387
MisterPalmx <palmz.hacker@gmail.com>
2018-04-02 23:54:04 +0700

    Add login system

--- 01/index.js
+++ 01/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const app = express();
+
 const PORT = 3000;
+const secretPassword = ['BIWZAZA', 'PALMMYMUMU', 'BOYREALLIFE', 'TEACHERWARODOM'];
 
 app.listen(PORT);
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+app.use(cookieParser('keyboard cat'));
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  if (secretPassword.indexOf(req.cookies.password) < 0) {
+    res.render('login');
+  } else {
+    res.render('index', {password: req.cookies.password});
+  }
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('password', req.body.password);
+  res.redirect('/');
+})
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('password');
+  res.redirect('/');
 })
 
 console.log('Server is running on port ' + PORT);
